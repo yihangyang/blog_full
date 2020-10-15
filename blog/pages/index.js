a4cd3795f6813c064df177383fc76ec0791e6c87
yihangyang <yangyihang@outlook.de>
2020-10-15 10:07:20 +0200

    build connection

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
 import { List, Row, Col } from 'antd'
-import { CalendarOutlined, StarTwoTone, YoutubeOutlined, FireOutlined } from '@ant-design/icons';
+import { CalendarOutlined, LikeFilled, YoutubeOutlined, FireOutlined } from '@ant-design/icons';
+import axios from 'axios'
 
 import Header from '../components/Header'
 import Author from '../components/Author'
@@ -11,15 +12,8 @@ import Footer from '../components/Footer'
 import '../static/style/pages/index.css'
 
 
-export default function Home() {
-  const [ mylist , setMylist ] = useState(
-    [
-      {title:'50',context:'50。'},
-      {title:'React',context:'50。'},
-      {title:'React',context:'50。'},
-      {title:'React',context:'50。'},
-     ]
-  )
+export default function Home(list) {
+  const [ mylist , setMylist ] = useState(list.data);
   return (
     <div>
       <Head>
@@ -37,11 +31,11 @@ export default function Home() {
               <List.Item>
                 <div className="list-title">{item.title}</div>
                 <div className="list-icon">
-                  <span><CalendarOutlined /> 2019-06-28</span>
-                  <span><YoutubeOutlined /> Video</span>
-                  <span><FireOutlined /> 5498 views</span>
+                  <span><CalendarOutlined /> {item.addTime}</span>
+                  <span><FireOutlined /> {item.typeName}</span>
+                  <span><LikeFilled/> {item.view_count}人</span>
                 </div>
-                <div className="list-context">{item.context}</div>  
+                <div className="list-context">{item.introduce}</div>  
               </List.Item>
             )}
           />    
@@ -56,4 +50,17 @@ export default function Home() {
       <Footer />
       </div>
   )
+}
+
+Home.getInitialProps = async () => {
+  const promise = new Promise((resolve)=>{
+    axios('http://127.0.0.1:7002/default/getArticleList').then(
+      (res)=>{
+        //console.log('远程获取数据结果:',res.data.data)
+        resolve(res.data)
+      }
+    )
+  })
+
+  return await promise
 }
